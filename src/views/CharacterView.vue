eefc238908a37434cf967cefaf995c883f4e4914
David Oliveira <daviddenisson2014@gmail.com>
2023-02-13 15:45:02 -0300

    ajustes na responsividade e adição de input na nova seção

--- src/views/CharacterView.vue
+++ src/views/CharacterView.vue
@@ -1,5 +1,19 @@
 <template>
   <main class="container search">
+
+    <form action="return false">
+        <div class="form-control">
+          <input type="text" class="form-input" placeholder="Type a Character" v-model="lookSearch">
+          <div class="input-options" v-if="searchResult != '' ">
+                 <div class="option" v-bind:key="index" v-for="(character, index) in searchResult" v-on:click="redirectCharacter(character)">
+                  {{ character.name }}
+                 </div>
+          </div>
+         
+        </div>
+        <button type="button"><i class="fa-solid fa-magnifying-glass"></i></button>
+      </form>
+
     <div class="card">
       <div class="card-image">
         <img v-bind:src="image" alt="character" />
@@ -43,7 +57,7 @@ export default {
       id: "",
       name: "",
       image: "",
-     
+      searchResult: "",
       status: "",
       especie: "",
       origem: "",
